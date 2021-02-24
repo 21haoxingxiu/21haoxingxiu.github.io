cc0768e8010c0901c57c46abc4b2713ed4a5db03
21haoxingxiu <1047918517@qq.com>
2021-02-24 18:05:40 +0800

    布局部分修改

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,17 +2,10 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: '前端',
+    text: '笔记',
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      {
-        text: '前端文章',
-        items: [
-          { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-          { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
-        ],
-      },
       {
         text: '学习笔记',
         items: [
@@ -36,18 +29,22 @@ module.exports = [
       },
     ],
   },
-  {
-    text: '页面',
-    link: '/ui/',
-    items: [
-      { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-      { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
-    ],
-  },
+  // {
+  //   text: '页面',
+  //   link: '/ui/',
+  //   items: [
+  //     { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
+  //     { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
+  //   ],
+  // },
   {
     text: '技术',
     link: '/technology/',
     items: [
+      { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
+      { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
+      { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
+      { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
       { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
       { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
       { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
@@ -62,10 +59,9 @@ module.exports = [
       { text: '面试', link: '/pages/aea6571b7a8bae86/' },
       { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
       { text: '实用技巧', link: '/pages/baaa02/' },
-      { text: '友情链接', link: '/friends/' },
     ],
   },
-  { text: '关于', link: '/about/' },
+  // { text: '关于', link: '/about/' },
   {
     text: '收藏',
     link: '/pages/beb6c0bd8a66cea6/',
