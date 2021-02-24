115dcefcc1011539365a85fe145abe4a11a8ac7d
21haoxingxiu <1047918517@qq.com>
2021-02-24 14:24:45 +0800

    首页信息修改

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -1,12 +1,13 @@
-const nav = require('./themeConfig/nav.js');
-const sidebar = require('./themeConfig/sidebar.js');
-const htmlModules = require('./themeConfig/htmlModules.js');
+const nav = require('./themeConfig/nav.js')
+const sidebar = require('./themeConfig/sidebar.js')
+const htmlModules = require('./themeConfig/htmlModules.js')
 
 // 主题配置
 module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-  logo: '/img/EB-logo.png', // 导航栏logo
+  logo:
+    'https://raw.githubusercontent.com/21haoxingxiu/picture/master/blog/logo.png', // 导航栏logo
   repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
@@ -47,13 +48,14 @@ module.exports = {
 
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: 'xugaoyi', // 必需
-    link: 'https://github.com/xugaoyi', // 可选的
+    name: 'zhanjiamin', // 必需
+    link: 'https://github.com/21haoxingxiu', // 可选的
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
-    avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-    name: 'Evan Xu',
+    avatar:
+      'https://raw.githubusercontent.com/21haoxingxiu/picture/master/blog/%E5%A4%B4%E5%83%8F.jpg',
+    name: 'jim zhan',
     slogan: '前端界的小学生',
   },
   social: {
@@ -63,17 +65,17 @@ module.exports = {
       {
         iconClass: 'icon-youjian',
         title: '发邮件',
-        link: 'mailto:894072666@qq.com',
+        link: 'mailto:1047918517@qq.com',
       },
       {
         iconClass: 'icon-github',
         title: 'GitHub',
-        link: 'https://github.com/xugaoyi',
+        link: 'https://github.com/21haoxingxiu',
       },
       {
         iconClass: 'icon-erji',
         title: '听音乐',
-        link: 'https://music.163.com/#/playlist?id=755597173',
+        link: 'https://www.clearlove666.com/h5-cloud-music-ts',
       },
     ],
   },
@@ -83,5 +85,5 @@ module.exports = {
     copyrightInfo:
       'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
   },
-  htmlModules // 插入hmtl(广告)模块
+  htmlModules, // 插入hmtl(广告)模块
 }
